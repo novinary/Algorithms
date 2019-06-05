09d0dab0d5d5af18b82c7991a43c294d47b5503f
novinary <novina.dev@gmail.com>
2019-06-05 16:13:36 +0100

    Finished write algorithm for recipe batches + tests are passing

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
 
+# Check if length of recipe is greater than length of ingredients, if it's true then return 0
+# For each item in recipe and ingredients, get a new count by diving no. of ingredient items by no. of recipe items
+# Check if initial count of 0 is less than new count or if count is equal to 0 then set initial count as new count and return count
+
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  count = 0
 
+  if len(recipe) > len(ingredients):    
+    return 0
+
+  # print(len(recipe),(ingredients))
+
+  for item in recipe and ingredients:
+    max = ingredients[item] // recipe[item]    #floor division to get the division result
+    # print(max)
+
+    if max < count or count == 0:
+      count = max
+    return count
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
