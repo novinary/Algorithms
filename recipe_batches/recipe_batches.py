d5f0c63408f9239d7f0035f854fa9f7ce18c503e
novinary <novina.dev@gmail.com>
2019-06-06 16:06:12 +0100

    Applied iterative solution for rps + tests passing for stock prices

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -26,5 +26,5 @@ if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 132, 'sugar': 48, 'flour': 51}
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python
-
+# Iterative solution
 import sys
 
-def rock_paper_scissors(n):
-  pass 
-
+def rock_paper_scissors(number):
+  rps = [['rock'],['paper'],['scissors']]
+  if number == 0:
+    return [[]]
+  elif number == 1:
+    return rps
+  else:
+    count = 2
+    plays={0:[[]],1:rps}
+    while count<=number:
+      last_play = plays[list(plays.keys())[-1]]
+      plays[count] = []
+      for i in range(len(last_play)):
+        last_play_item=last_play[i][0:]
+        last_play_item.append('rock')
+        plays[count].append(last_play_item)
+        last_play_item=last_play_item[0:-1]
+        last_play_item.append('paper')
+        plays[count].append(last_play_item)
+        last_play_item=last_play_item[0:-1]
+        last_play_item.append('scissors')
+        plays[count].append(last_play_item)
+      count+=1
+  return plays[number]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
